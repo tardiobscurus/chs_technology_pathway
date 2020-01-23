0dae31d1c8e2c8dce1f2fa4d0eeb53758804b755
Hansel Chavez <hchavezblanco@hotmail.com>
2020-01-22 23:41:32 -0700

    Added,

---
+++ script.js
@@ -0,0 +1,36 @@
+const hamburger     = document.querySelector(".hamburger-nav");
+
+hamburger.addEventListener("click", () => {
+    alert("I've been clicked!")
+});
+
+window.addEventListener("scroll", () => {
+    const title         = document.querySelector(".title");
+    const image         = document.querySelector(".launch-back img");
+    const line          = document.querySelectorAll(".hamburger-nav .line");
+    var scroll          = window.pageYOffset;
+    var rate            = scroll * .3;
+    var backrate        = scroll * .7;
+
+    console.log(scroll);
+
+    title.style.transform = `translate3d(0, ${rate}px, 0)`;
+    image.style.transform = `translate3d(0, ${backrate}px, 0)`;
+
+    if (scroll > 625) {
+        line[0].style.backgroundColor = "black";
+        line[1].style.backgroundColor = "black";
+        line[2].style.backgroundColor = "black";
+    } else {
+        line[0].style.backgroundColor = "white";
+        line[1].style.backgroundColor = "white";
+        line[2].style.backgroundColor = "white";
+    }
+});
+
+/*
+
+TODO:
+- Create a FOR loop on line 21 - 23 & 25 - 27 for better reading on the code.
+
+*/
