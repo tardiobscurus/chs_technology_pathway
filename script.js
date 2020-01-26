03ab8486850845bbc30ce14e4a23cf3a07e04dd5
Hansel Chavez <hchavezblanco@hotmail.com>
2020-01-25 18:16:15 -0700

    Added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+    console.log(window.innerWidth);
     var isNotOpen       = true;
     const navigation    = document.querySelector(".navigation");
     const hamburger     = document.querySelector(".hamburger-nav");
@@ -10,6 +11,7 @@ window.onload = function() {
         if (!isNotOpen) {
             isNotOpen = true;
             navigation.style.opacity = "0";
+            navigation.style.transform = `translate3d(0, 100vh, 0)`;
             navigation.style.pointerEvents = "none";
         
             for (var i = 0; i < 3; i++) {
@@ -20,23 +22,27 @@ window.onload = function() {
         } else {
             isNotOpen = false;
             navigation.style.opacity = "1";
+            navigation.style.transform = `translate3d(0, 0, 0)`;
             navigation.style.pointerEvents = "auto";
 
-            for (var i = 0; i < 3; i++) {
-                if (line[i].style.backgroundColor = "black") {
-                    line[i].style.backgroundColor = "white";
+            if(window.pageYOffset >= (window.innerHeight - 20)) {
+                for (var i = 0; i < 3; i++) {
+                    if (line[i].style.backgroundColor = "black") {
+                        line[i].style.backgroundColor = "white";
+                    }
                 }
             }
         }
     });
 
+    const image         = document.querySelector(".launch-back img");
+
     window.addEventListener("scroll", () => {
         const title         = document.querySelector(".title");
-        const image         = document.querySelector(".launch-back img");
         const line          = document.querySelectorAll(".hamburger-nav .line");
         var scroll          = window.pageYOffset;
         var rate            = scroll * .3;
-        var backrate        = scroll * .7;
+        var backrate        = scroll * .5;
 
         title.style.transform = `translate3d(0, ${rate}px, 0)`;
         image.style.transform = `translate3d(0, ${backrate}px, 0)`;
@@ -50,5 +56,27 @@ window.onload = function() {
                 line[i].style.backgroundColor = "white";
             }
         }
+
+        ////////////////////////////////////////////////////////
+
+        const contentIMGOne = document.querySelector(".content1 img");
+        const contentTEXTOne = document.querySelector(".content1 .text");
+        
+        if (window.innerWidth > 1000) {
+            contentIMGOne.style.transform = `translate3d(-${scroll * .1}px, 0, 0)`;
+            contentTEXTOne.style.padding = `${(scroll * .05) + 20}px 40px`;
+            contentTEXTOne.style.transform = `translate3d(${scroll * .05}px, 0, 0)`;
+            contentTEXTOne.style.borderRadius = `${(scroll * .01)}px`;
+        }
+
     });
+
+    var random = Math.floor(Math.random() * 3);
+    if (random == 0) {
+        image.src = "images/gifs/spheres-dots.webp";
+    } else if (random == 1) {
+        image.src = "images/gifs/spiral-dots.gif";
+    } else if (random == 2) {
+        image.src = "images/gifs/weird-movement.gif";
+    }
 }
